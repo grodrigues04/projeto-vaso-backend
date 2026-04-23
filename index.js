19d9ba01d0c1d53810cc5f5e27742d0955703fed
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2026-04-22 22:44:21 -0300

    alterações nas rotas

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-const db = require('./db.js');
-app.use(cors()); // libera qualquer origem
-app.use(express.json());
+
+app.use(cors());
+app.use(express.json({ limit: '10kb' }));
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  console.log(req.body);
+  next();
+});
 
 app.post('/umidade', (req, res) => {
+  if (!req.body || typeof req.body.umidade !== 'number') {
+    return res.status(400).send('invalid body');
+  }
+
   console.log('Dados recebidos:', req.body);
   res.sendStatus(200);
 });
 
-app.get('/teste', async (req, res) => {
-    console.log('Teste de rota');
-    res.sendStatus(200);
+app.get('/teste', (req, res) => {
+    console.log("Ping!")
+  res.sendStatus(200);
 });
 
 app.listen(3000, '0.0.0.0', () => {
